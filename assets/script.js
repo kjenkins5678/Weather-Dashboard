4e71812de2e05a8a001de93d8fad7e6eac4e9d5f
kjenkins5678 <kajenkins9@gmail.com>
2019-12-18 21:52:51 -0500

    enter city, city is then used in api search, results written back to div

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,40 @@
-// This is our API key
-var APIKey = "d93007fc75acd09b861e4011b5d15c06";
-var city = "District of Columbia"
+var cities = [];
 
-// Here we are building the URL we need to query the database
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-  "q=" + city + "&units=imperial&appid=" + APIKey;
+$(".btn").on("click", function() {
 
-// Here we run our AJAX call to the OpenWeatherMap API
-$.ajax({
-  url: queryURL,
-  method: "GET"
-})
-  // We store all of the retrieved data inside of an object called "response"
-  .then(function(response) {
+  event.preventDefault();
+  city = $("#cityInput").val();
+  cities.push(city);
+  console.log(cities);
 
-    // Log the queryURL
-    console.log(queryURL);
+  $(".citySearches").append($("<div class='searched'>").text(city));
 
-    // Log the resulting object
-    console.log(response);
 
-  });
+  var APIKey = "d93007fc75acd09b861e4011b5d15c06";
+
+  // Here we are building the URL we need to query the database
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+    "q=" + city + "&units=imperial&appid=" + APIKey;
+
+  // Here we run our AJAX call to the OpenWeatherMap API
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    // We store all of the retrieved data inside of an object called "response"
+    .then(function(response) {
+
+      // Log the queryURL
+      console.log(queryURL);
+
+      // Log the resulting object
+      console.log(response.name);
+
+      var currentConditions = $("<div class='currentConditions'>");
+      var ccName = $("<p class='name'>").text(response.name);
+      currentConditions.append(ccName);
+      $(".weatherDisplay").append(currentConditions);
+
+    });
+
+});
